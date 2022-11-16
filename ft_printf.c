d9e9d9a898a28d2b917efef1d51f1ddd0210a454
Elias Zanotti <ezanotti@z2r10p8.42lyon.fr>
2022-11-16 16:42:41 +0100

    add files

--- ft_printf.c
+++ ft_printf.c
@@ -6,60 +6,62 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:15:46 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/15 20:50:28 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/16 16:33:06 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-#include <stdio.h>
+static int	ft_putnbr_unsigned(unsigned int n)
+{
+	if (n >= 0 && n <= 9)
+		return (ft_putchar(n + '0'));
+	return (ft_putnbr(n / 10) + ft_putchar((n % 10) + '0'));
+}
 
+static int	ft_printf_calls(va_list args, char type)
+{
+	int	total;
 
+	total = 0;
+	if (type == 'c')
+		total += ft_putchar(va_arg(args, int));
+	else if (type == 's')
+		total += ft_putstr(va_arg(args, char *));
+	else if (type == 'p')
+		total += ft_printaddress(va_arg(args, void *));
+	else if (type == 'd')
+		total += ft_putnbr(va_arg(args, int));
+	else if (type == 'i')
+		total += ft_putnbr(va_arg(args, int));
+	else if (type == 'u')
+		total += ft_putnbr_unsigned(va_arg(args, int));
+	else if (type == 'x')
+		total += ft_convert_base("0123456789abcdef", va_arg(args, int));
+	else if (type == 'X')
+		total += ft_convert_base("0123456789ABCDEF", va_arg(args, int));
+	else if (type == '%')
+		total += ft_putchar('%');
+	return (total);
+}
 
-int ft_printf(const char *s, ...)
+int	ft_printf(const char *s, ...)
 {
 	va_list	args;
 	int		i;
+	int		total;
 
 	va_start(args, s);
 	i = 0;
+	total = 0;
 	while (s[i])
-    {
-        if (s[i] == '%' && s[i + 1])
-        {
-			i++;
-            if (s[i] == 'c')
-				ft_putchar(va_arg(args, int));
-			else if (s[i] == 's')
-				ft_putstr(va_arg(args, char *));
-			else if (s[i] == 'p')
-				ft_printaddress(va_arg(args, void *));
-			else if (s[i] == 'd')
-				ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'i')
-				ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'u')
-				ft_putnbr(va_arg(args, int));
-			else if (s[i] == 'x')
-                ft_convert_base("0123456789abcdef", va_arg(args, int));
-			else if (s[i] == 'X')
-                ft_convert_base("0123456789ABCDEF", va_arg(args, int));
-			else if (s[i] == '%')
-				ft_putchar('%');
-        }
+	{
+		if (s[i] == '%' && s[i + 1])
+			total += ft_printf_calls(args, s[++i]);
 		else
-			ft_putchar(s[i]);
+			total += ft_putchar(s[i]);
 		i++;
-    }
+	}
 	va_end(args);
-	return (0);
-}
-
-#include <stdio.h>
-int main()
-{
-	char *a = "denufhue";
-
-	printf("%p\n", &a);
-	ft_printf("%p\n", &a);
+	return (total);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,20 +6,23 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:16:33 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/15 20:51:31 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/16 16:40:22 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <limits.h>
 
-void	ft_putchar(char c);
-void	ft_putstr(char *s);
-void	ft_putnbr(int n);
-void	ft_printaddress(void *address);
-void	ft_convert_base(char *base, int nbr);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_printaddress(void *address);
+int	ft_convert_base(char *base, long int nbr);
+
+int	ft_printf(const char *s, ...);
 
 #endif
