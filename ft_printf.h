cbb59dab3a23f62d736a10405bd0fa54cec48617
Elias Zanotti <ezanotti@z2r9p2.42lyon.fr>
2022-11-22 13:34:42 +0100

    nrm

--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: ezanotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 18:16:33 by ezanotti          #+#    #+#             */
-/*   Updated: 2022/11/18 14:50:40 by ezanotti         ###   ########lyon.fr   */
+/*   Updated: 2022/11/22 13:34:27 by ezanotti         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPRINTF_H
-# define LIBFTPRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H 
 
 # include <unistd.h>
 # include <stdarg.h>
